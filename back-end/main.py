7c85989e3d7c8d589b76f435bfc2844fb58ed624
jinseonggram <king7762225@gmail.com>
2022-11-04 09:31:37 +0900

    deploy : Deploy Api server

--- back-end/main.py
+++ back-end/main.py
@@ -1,30 +1,108 @@
-from flask import Flask
-from ddb import DDB
+from flask import Flask, jsonify
 from dotenv import load_dotenv
 import os
 from elasticsearch import Elasticsearch
+from flask_cors import CORS
 
 load_dotenv()
 
 app = Flask(__name__)
 
+load_dotenv()
+host = os.environ.get('host')
+user = os.environ.get('user')
+password = os.environ.get('password')
+es = Elasticsearch(host, http_auth=(user, password))
 
-es = Elasticsearch('[엘라스틱_서버_IP_주소]:9200')
+app = Flask(__name__)
+CORS(app)
+app.config['JSON_AS_ASCII'] = False
+app.config['CORS_HEADERS'] = 'Content-Type'
+es_index = 'news'
+category_dict = {
+    'news': '뉴스',
+    'sports': '스포츠',
+    'life': '생활',
+    'money': '경제',
+    'tech': '기술',
+    'travel': '여행',
+    'opinion': '기타'
+}
 
-# 일반적인 라우트 방식입니다.
-@app.route('/api/article/<category>')
-def board(category):
-    return "그냥 보드"
+ko_category_dict = {
+    '뉴스':'news',
+    '스포츠':'sports',
+    '생활': 'life',
+    '경제':'money',
+    '기술':'tech',
+    '여행':'travel',
+    '기타':'opinion',
+}
 
-# URL 에 매개변수를 받아 진행하는 방식입니다.
+
+@app.route('/api/articles/<category>')
+def article_view(category):
+    news_list = []
+    body = {
+        "size": 100,  # max get value count
+        "sort": [
+            {"date": {"order": "asc"}},
+        ],
+        "query": {
+            "match": {
+                "category": ko_category_dict[category]
+            }
+        }
+    }
+    res = es.search(index=es_index, body=body)
+    article_list = res['hits']['hits']
+    for i in article_list:
+        try:
+            news_format = {
+                'article_id': i['_source']['uid'],
+                'date': i['_source']['date'],
+                'platform': i['_source']['platform'],
+                'en_category': i['_source']['category'].split(',')[0],
+                'en_headline': i['_source']['en_headline'],
+                'ko_category': category_dict[i['_source']['category'].split(',')[0]],
+                'ko_headline': i['_source']['ko_headline'],
+            }
+            news_list.append(news_format)
+        except Exception as e:
+            print('에러', e)
+    return jsonify({'data': news_list})
+
+
 @app.route('/api/article/<article_id>')
-def board_view(article_id):
-    return article_id
+def article_detail(article_id):
+    body = {
+        "size": 100,  # max get value count
+        "query": {
+            "match": {
+                "uid": article_id
+            }
+        }
+    }
+    res = es.search(index=es_index, body=body)
+    article_list = res['hits']['hits']
+    print(res)
+    i = article_list[0]
+    article = {
+        'article_id': i['_source']['uid'],
+        'date': i['_source']['date'],
+        'platform': i['_source']['platform'],
+        'en_category': i['_source']['category'].split(',')[0],
+        'en_headline': i['_source']['en_headline'],
+        'ko_category': category_dict[i['_source']['category'].split(',')[0]],
+        'ko_headline': i['_source']['ko_headline'],
+        'en_content': i['_source']['en_content'],
+        'en_summarization': i['_source']['en_summarization'],
+        'ko_content': i['_source']['ko_content'],
+        'ko_summarization': i['_source']['ko_summarization'],
+        'link': 'https://www.usatoday.com/story/news/politics/2022/11/03/pelosi-released-hospital-hammer-attack-depape/8261279001/'
+    }
 
-# 위에 있는것이 Endpoint 역활을 해줍니다.
-@app.route('/boards',defaults={'page':'index'})
-@app.route('/boards/<page>')
-def boards(page):
-    return page+"페이지입니다."
+    return jsonify({'data': article})
+
 
 app.run(host="localhost",port=5001,debug=True)
